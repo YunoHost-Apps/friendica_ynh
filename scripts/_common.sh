f31d1016bea8192d3b06f6c15ea0ce724c48d288
anmol26s <5068843+anmol26s@users.noreply.github.com>
2018-09-28 22:00:45 +0530

    Upgraded to 2018.09  (#12)
    * Updated to version 2018.09 (#11)
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Upgrade to 2018.09
    
    * Fixed ldap plugin in 2018.09
    
    * Removed name from ldap configuration
    
    * Updation from previous version fix
    
    * Updation from previous version fix
    
    * Updation from previous version fix
    
    * Put commenting at right palce

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,3 +12,37 @@ ynh_delete_file_checksum () {
 	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
 	ynh_app_setting_delete $app $checksum_setting_name
 }
+
+# =============================================================================
+# COMMON ROUNDCUBE FUNCTIONS
+# =============================================================================
+
+# Execute a composer command from a given directory
+# usage: composer_exec workdir COMMAND [ARG ...]
+exec_composer() {
+  local workdir=$1
+  shift 1
+
+  COMPOSER_HOME="${workdir}/.composer" \
+    php "${workdir}/composer.phar" $@ \
+      -d "${workdir}" --quiet --no-interaction
+}
+
+# Install and initialize Composer in the given directory
+# usage: init_composer destdir
+init_composer() {
+  local destdir=$1
+
+  # install composer
+  curl -sS https://getcomposer.org/installer \
+    | COMPOSER_HOME="${destdir}/.composer" \
+        php -- --quiet --install-dir="$destdir" \
+    || ynh_die "Unable to install Composer"
+
+  # install composer.json
+  cp "${destdir}/composer.json-dist" "${destdir}/composer.json"
+
+  # update dependencies to create composer.lock
+  exec_composer "$destdir" install --no-dev \
+    || ynh_die "Unable to update Roundcube core dependencies"
+}
