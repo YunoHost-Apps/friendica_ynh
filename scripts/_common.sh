55fd23cd2a2a65418dd6d65146aa83d6c5a5a593
ericgaspar <junk.eg@free.fr>
2021-02-03 19:50:12 +0100

    Fix linter

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,4 +8,4 @@
 YNH_PHP_VERSION="7.3"
 pkg_dependencies=""
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd"
+extra_php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd"
