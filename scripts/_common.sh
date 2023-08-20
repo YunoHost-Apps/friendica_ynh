f360d9ae3bc43d21c190c6f544f9d0d250e4e97c
ericgaspar <junk.eg@free.fr>
2023-08-20 15:07:34 +0200

    add gmp module

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,7 +12,7 @@ addons_version_commit="f922d69310f5229d2e9404f396d66453b2d19d90"
 # dependencies used by the app
 YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd"
+pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-gmp"
 
 #=================================================
 # EXPERIMENTAL HELPERS
