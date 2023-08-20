c56aff9ea1283daedae22459edaf193ea2cea65e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-20 15:36:10 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,9 +10,9 @@ version_commit="2dbfb070083ec395bf5d24ec89fe96b282c6a12d"
 addons_version_commit="f2cc0312ca9a95d99a8330452848180792bf9227"
 
 # dependencies used by the app
-YNH_PHP_VERSION="7.4"
+#REMOVEME? YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-gmp"
+#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-gmp"
 
 #=================================================
 # EXPERIMENTAL HELPERS
