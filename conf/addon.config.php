f3168788a66d4b40a0d8ea311f75bdeaa1097b57
OniriCorpe <oniricorpe@disroot.org>
2024-02-22 05:04:11 +0100

    Daemon (#127)
    * add schema
    
    * add daemon
    
    * finalization
    
    * fixes
    
    * systemd config renamed to $app-daemon
    
    * fixes
    
    * fixes
    
    * gorgotten yunohost service remove "$app"
    
    * fixes
    
    * fix StandardOutput
    
    * fix this damn pidfile
    
    * better comment for posterity
    
    * fix yunohost service remove
    
    * update config files
    
    * fix domain name change
    
    * use the provided config file template at install
    
    * Auto-update README
    
    * post install: show explicitely the admin login (email)
    
    * trying to fix the systemd config
    
    * fix pidfile config indentation
    
    * add log path
    
    * remove irrelevant comment
    
    * delete an eventual remaining daemon.pid at restoration
    
    * fixes
    
    * fix a typo for all_users
    
    * adding comment: "Removing the cron..."
    
    * use ynh_secure_remove instead of rm
    
    * add pre upgrade message about the daemon
    
    * fix service name
    
    * comment
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- conf/addon.config.php
+++ conf/addon.config.php
@@ -1,6 +1,7 @@
 <?php
 // Addon configuration
-// Copy this configuration file to addon.config.php and edit it if you want to configure addons, see below example for the twitter addon
+// LDAP config for YunoHost
+
 return [
 	'ldapauth' => [
 		'ldap_server' => 'localhost',
--- conf/local-sample.config.php
+++ conf/local-sample.config.php
@@ -1,20 +1,8 @@
 <?php
 
 // Local configuration
+// Docs: https://github.com/friendica/friendica/blob/stable/static/settings.config.php
 
-/* If automatic system installation fails:
- *
- * Copy this file to local.config.php
- *
- * Why local.config.php? Because it contains sensitive information which could
- * give somebody complete control of your database. Apache's default
- * configuration will interpret any .php file as a script and won't show the values
- *
- * Then set the following for your MySQL installation
- *
- * If you're unsure about what any of the config keys below do, please check the static/defaults.config.php file for
- * detailed documentation of their data type and behavior.
- */
 
 return [
 	'database' => [
@@ -37,11 +25,17 @@ return [
 		'sitename' => 'Friendica Social Network',
 		'register_policy' => \Friendica\Module\Register::OPEN,
 		'max_import_size' => 200000,
+		'register_text' => '',
 	],
 	'system' => [
 		'url' => 'https://__DOMAIN__',
 		'basepath' => '__INSTALL_DIR__',
 		'default_timezone' => '__TIMEZONE__',
 		'language' => '__LANGUAGE__',
+		'pidfile' => '__INSTALL_DIR__/daemon.pid',
+		// Blocks search for users who are not logged in to prevent crawlers from blocking your system.
+		'local_search' => false,
+		'logfile' => '/var/log/friendica/friendica.log',
+		'loglevel' => 'notice',
 	],
 ];
